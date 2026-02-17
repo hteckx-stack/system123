b821f02827fa189558ba89a7a696e304eec94076
Blaq-X-manhua <blaqxmanhua@gmail.com>
2026-02-17 09:51:44 +0000

    make the screen be tablet mood and able to fit the screen

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from "./_components/sidebar"
 import { AppHeader } from "./_components/header"
 
@@ -11,9 +11,9 @@ export default function DashboardLayout({
     <SidebarProvider>
       <div className="flex min-h-screen w-full">
         <AppSidebar />
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col overflow-hidden">
           <AppHeader />
-          <main className="flex-1 p-4 sm:px-6 sm:py-0">
+          <main className="flex-1 overflow-auto p-4 sm:p-6">
             {children}
           </main>
         </div>
